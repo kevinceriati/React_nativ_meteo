d4fb404b3cf8fc46c8a6e01fe11c0d91daecf773
Kevin <kevin.ceriati@le-campus-numerique.fr>
2019-09-25 16:18:49 +0200

    'update'

---
+++ models/appModel.js
@@ -0,0 +1,12 @@
+export const app = {
+    state: {
+        name:'Pouet',
+    },
+    reducers: {
+        setName(state, name){
+         return {...state, name};
+        },
+    },
+    effects:{},
+};
+
---
+++ navigation/AppNavigator.js
@@ -0,0 +1,27 @@
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import {  createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import HomeScreen from "../screens/HomeScreen";
+import AuthLoadingScreen from "../screens/AuthLoadingScreen";
+import IntroScreen from "../screens/IntroScreen";
+import IntroFormScreen from "../screens/IntroFormScreen";
+
+const AppStack = createBottomTabNavigator({
+    Home: HomeScreen,
+    Intro: IntroScreen
+});
+
+const AuthStack = createStackNavigator({
+    SignIn: IntroFormScreen,
+    Welcome: IntroScreen
+});
+
+const SwitchNavigator = createSwitchNavigator({
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+},{
+    initialRouteName: "AuthLoading",
+});
+
+export default createAppContainer(SwitchNavigator);
